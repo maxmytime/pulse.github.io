e1620eafd658f8583b0acf85245101f2c79989b7
maxmytime <maxmytime@yandex.ru>
2021-01-11 23:16:11 +0700

    Добавлин адаптив секции .catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        variableWidth: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
               settings: {
-                dots: true,
+                dots: false,
                 arrows: false
               }
             },
