4065051365b5aa8b74c17af5203a5d4d010347b0
maxmytime <maxmytime@yandex.ru>
2021-01-17 23:47:19 +0700

    Добавлен скрипт показа/скрытия модальных окон

--- src/js/script.js
+++ src/js/script.js
@@ -15,11 +15,15 @@ $(document).ready(function(){
           ]
       });
 
+    // Tab
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+    // catalog-item slide
 
     function toggleSlide(item) {
         $(item).each(function(i) {
@@ -34,5 +38,21 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 
 });
